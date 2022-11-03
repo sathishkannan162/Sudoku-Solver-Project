aec01c40a2de073b49aa7df82d0c4671a9525916
sathishkannan162 <sathishkannan162@gmail.com>
2022-11-03 15:35:06 +0530

    feat: validate string in sudoku solver

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,24 +1,23 @@
 class SudokuSolver {
-
   validate(puzzleString) {
+    if (/^[\d\.]*$/.test(puzzleString)) {
+      if (puzzleString.length == 81) {
+        return 'valid string'
+      } else {
+        return 'String length should be 81 characters'
+      }
+    } else {
+      return 'invalid characters'
+    }
   }
 
-  checkRowPlacement(puzzleString, row, column, value) {
+  checkRowPlacement(puzzleString, row, column, value) {}
 
-  }
+  checkColPlacement(puzzleString, row, column, value) {}
 
-  checkColPlacement(puzzleString, row, column, value) {
+  checkRegionPlacement(puzzleString, row, column, value) {}
 
-  }
-
-  checkRegionPlacement(puzzleString, row, column, value) {
-
-  }
-
-  solve(puzzleString) {
-    
-  }
+  solve(puzzleString) {}
 }
 
-module.exports = SudokuSolver;
-
+module.exports = SudokuSolver
