bc7e4f27eea5053067cec36b021e3796fa78f6a8
sathishkannan162 <sathishkannan162@gmail.com>
2022-11-03 15:11:16 +0530

    test: unit tests for sudoku solver

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -20,43 +20,43 @@ suite('Unit Tests', () => {
     });
     suite('validate row placements', function(){
         test('Handle invalid row placement',function(){
-
+            assert.isFalse(SudokuSolver.checkRowPlacement(puzzlesAndSolutions[0][0],C,3,5));
         });
         test('Handle valid row placement',function(){
-
+            assert.isTrue(SudokuSolver.checkRowPlacement(puzzlesAndSolutions[0][0],C,3,8));
         });
     });
 
     suite('validate column placements', function(){
         test('Handle invalid column placement',function(){
-
+            assert.isFalse(SudokuSolver.checkColplacement(puzzlesAndSolutions[0][0],C,3,6));
         });
         test('Handle valid column placement',function(){
-
+            assert.isTrue(SudokuSolver.checkColplacement(puzzlesAndSolutions[0][0],C,3,8));
         });
     })
     suite('validate region placements', function(){
         test('Handle invalid region placement',function(){
-
+            assert.isFalse(SudokuSolver.checkRegionPlacement(puzzlesAndSolutions[0][0],C,3,1));
         });
         test('Handle valid region placement',function(){
+            assert.isTrue(SudokuSolver.checkRegionPlacement(puzzlesAndSolutions[0][0],C,3,8));
 
         });
     })
     suite('Pass strings to solver', function(){
         test('invalid string fails the solver',function(){
-
+            assert.equal(SudokuSolver.solve('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..168...926914.37.'),'not solvable');
         });
         test('Valid string passes the solver',function(){
-
+            assert.notEqual(SudokuSolver.solve(puzzlesAndSolutions[0][0]),'not solvable');
         });
         
     })
     suite('Return solutions', function(){
         test('Return solution for incomplete puzzle',function(){
-
+            assert.equal(puzzlesAndSolutions[0][0],puzzlesAndSolutions[0][1]);
         });
-        
     })
     
 });
