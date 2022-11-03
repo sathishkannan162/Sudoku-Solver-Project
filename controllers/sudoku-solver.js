7fa624f23640d62f500cb24de2142368be35aa70
sathishkannan162 <sathishkannan162@gmail.com>
2022-11-03 17:16:34 +0530

    feat: check region placement

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -2,43 +2,62 @@ class SudokuSolver {
   validate(puzzleString) {
     if (/^[\d\.]*$/.test(puzzleString)) {
       if (puzzleString.length == 81) {
-        return 'valid string'
+        return 'valid string';
       } else {
-        return 'String length should be 81 characters'
+        return 'String length should be 81 characters';
       }
     } else {
-      return 'invalid characters'
+      return 'invalid characters';
     }
   }
   getRowNum(letter) {
-    let rows = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
-    return rows.indexOf(letter.toLowerCase()) + 1
+    let rows = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+    return rows.indexOf(letter.toLowerCase()) + 1;
   }
-  getColValues(puzzleString,col) {
-    let colValues = []
-    for (let i=0; i<9; i++) {
+  getColValues(puzzleString, col) {
+    let colValues = [];
+    for (let i = 0; i < 9; i++) {
       // col -1 is used as index starts from zero.
-      colValues.push(puzzleString[i*9+(col -1)]);
+      colValues.push(puzzleString[i * 9 + (col - 1)]);
     }
     return colValues.join('');
   }
+
+  getRegionNum(row, col) {
+    let rowNum = this.getRowNum(row);
+    return [Math.ceil(rowNum / 3), Math.ceil(col / 3)];
+  }
+
+  getRegionValues(puzzleString, reg) {
+    // note puzzleString starts with index 0
+    let regColVals = puzzleString.slice((reg[0] - 1) * 3 * 9, reg[0] * 3 * 9);
+    let regValues = [];
+    for (let i = 0; i < 3; i++) {
+      regValues.push(regColVals[9*i+(reg[1]-1)*3],regColVals[9*i+(reg[1]-1)*3+1],regColVals[9*i+(reg[1]-1)*3+2])
+    }
+    return regValues.join('');
+    }
+
   checkRowPlacement(puzzleString, row, column, value) {
-    let rowNum = this.getRowNum(row)
+    let rowNum = this.getRowNum(row);
     // note string postions start from index 0 and slice slices start index and leaves end index.
     // They nullify in calculations
-    let startNum = (rowNum - 1) * 9
-    let rowValues = puzzleString.slice(startNum, startNum + 9)
+    let startNum = (rowNum - 1) * 9;
+    let rowValues = puzzleString.slice(startNum, startNum + 9);
     return !rowValues.includes(value);
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-    let colValues = this.getColValues(puzzleString,column);
+    let colValues = this.getColValues(puzzleString, column);
     return !colValues.includes(value);
   }
 
-  checkRegionPlacement(puzzleString, row, column, value) {}
+  checkRegionPlacement(puzzleString, row, column, value) {
+    let regVals = this.getRegionValues(puzzleString,this.getRegionNum(row,column));
+    return !regVals.includes(value);
+  }
 
   solve(puzzleString) {}
 }
 
-module.exports = SudokuSolver
+module.exports = SudokuSolver;
