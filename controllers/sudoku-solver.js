6d2b91edd49e0a11bb66da6fcd0c5e1672d7890b
sathishkannan162 <sathishkannan162@gmail.com>
2022-11-03 15:59:29 +0530

    feat: check row placement

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -10,8 +10,19 @@ class SudokuSolver {
       return 'invalid characters'
     }
   }
+  getRowNum(letter) {
+    let rows = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h']
+    return rows.indexOf(letter.toLowerCase()) + 1
+  }
 
-  checkRowPlacement(puzzleString, row, column, value) {}
+  checkRowPlacement(puzzleString, row, column, value) {
+    let rowNum = this.getRowNum(row)
+    // note string postions start from index 0 and slice slices start index and leaves end index.
+    // They nullify in calculations
+    let startNum = (rowNum - 1) * 9
+    let rowValues = puzzleString.slice(startNum, startNum + 9)
+    return !rowValues.includes(value)
+  }
 
   checkColPlacement(puzzleString, row, column, value) {}
 
