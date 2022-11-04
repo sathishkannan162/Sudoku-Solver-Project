bedf2cd5ba0653be3ee45a44a72035a7aceacb22
sathishkannan162 <sathishkannan162@gmail.com>
2022-11-04 17:06:06 +0530

    feat: add logic for /api/check

--- routes/api.js
+++ routes/api.js
@@ -5,10 +5,72 @@ const SudokuSolver = require('../controllers/sudoku-solver.js');
 module.exports = function (app) {
   let solver = new SudokuSolver();
 
-  app.route('/api/check').post((req, res) => {});
+  app.route('/api/check').post((req, res) => {
+    if (
+      req.body.puzzle !== '' &&
+      req.body.coordinate !== '' &&
+      req.body.value !== '' &&
+      req.body.hasOwnProperty('puzzle') &&
+      req.body.hasOwnProperty('coordinate') &&
+      req.body.hasOwnProperty('value')
+    ) {
+      if (/^[a-f]\d$/i.test(req.body.coordinate)) {
+        if (/^[\.\d]*$/.test(req.body.puzzle)) {
+          if (req.body.puzzle.length == 81) {
+            if (/^\d$/.test(req.body.value)) {
+              // parse coordinate
+              let coordinates = [...req.body.coordinate];
+              let strPos = solver.getPosInString(coordinates[0],coordinates[1]);
+              if (/\d/.test(req.body.puzzle[strPos])) {
+                // req.body.puzzle[solver.getPosInString(coordinates[0],coordinates[1])] == '.'
+                let newPuzzle = req.body.puzzle.slice(0,strPos)+'.'+req.body.puzzle.slice(strPos+1);
+                req.body.puzzle = newPuzzle;
+              }
+              let conflict = solver.checkConflict(
+                req.body.puzzle,
+                coordinates[0],
+                coordinates[1],
+                req.body.value
+              );
+              if (conflict.length == 0) {
+                res.json({
+                  valid: true,
+                });
+              } else {
+                res.json({
+                  valid: false,
+                  conflict: conflict,
+                });
+              }
+            } else {
+              res.json({
+                error: 'Invalid value',
+              });
+            }
+          } else {
+            res.json({
+              error: 'Expected puzzle to be 81 characters long',
+            });
+          }
+        } else {
+          res.json({
+            error: 'Invalid characters in puzzle',
+          });
+        }
+      } else {
+        res.json({
+          error: 'Invalid coordinate',
+        });
+      }
+    } else {
+      res.json({
+        error: 'Required field(s) missing',
+      });
+    }
+  });
 
   app.route('/api/solve').post((req, res) => {
-    console.log(req.body);
+    
     if (req.body.puzzle !== '' && req.body.hasOwnProperty('puzzle')) {
       if (/^[\.\d]*$/.test(req.body.puzzle)) {
         if (req.body.puzzle.length == 81) {
