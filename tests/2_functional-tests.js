f834904a551982212673a864d168291d9a1d56a9
sathishkannan162 <sathishkannan162@gmail.com>
2022-11-04 11:45:55 +0530

    test: plan all tests

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -1,11 +1,70 @@
-const chai = require("chai");
+const chai = require('chai');
 const chaiHttp = require('chai-http');
 const assert = chai.assert;
 const server = require('../server');
+let puzzlesAndSolutions = require('../controllers/puzzle-strings');
 
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
-
+  suite('POST /api/solve => ', function () {
+    test('POST with valid puzzle string', function (done) {
+      assert.fail();
+      done();
+    });
+    test('POST with missing puzzle string', function (done) {
+      assert.fail();
+      done();
+    });
+    test('POST with invalid characters in puzzle', function (done) {
+      assert.fail();
+      done();
+    });
+    test('POST with string incorrect length', function (done) {
+      assert.fail();
+      done();
+    });
+    test('POST with puzzle not solvable', function (done) {
+      assert.fail();
+      done();
+    });
+  });
+  suite('POST /api/check => ', function () {
+    test('POST with valid placement with no conflicts', function (done) {
+      assert.fail();
+      done();
+    });
+    test('POST with valid placement with one placement conflicts', function (done) {
+      assert.fail();
+      done();
+    });
+    test('POST with valid placement with multiple placement conflicts', function (done) {
+      assert.fail();
+      done();
+    });
+    test('POST with valid placement with all placement conflicts', function (done) {
+      assert.fail();
+      done();
+    });
+    test('POST with missing required fields', function (done) {
+      assert.fail();
+      done();
+    });
+    test('POST with invalid string characters', function (done) {
+      assert.fail();
+      done();
+    });
+    test('POST with incorrect string length', function (done) {
+      assert.fail();
+      done();
+    });
+    test('POST with invalid placement coordinate', function (done) {
+      assert.fail();
+      done();
+    });
+    test('POST with invalid placement value', function (done) {
+      assert.fail();
+      done();
+    });
+  });
 });
-
