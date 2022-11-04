8cb9fba78b78a35d8abbea8c000371a85df8f848
sathishkannan162 <sathishkannan162@gmail.com>
2022-11-04 15:29:50 +0530

    feat: handle /api/solve

--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,39 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
   let solver = new SudokuSolver();
 
-  app.route('/api/check')
-    .post((req, res) => {
+  app.route('/api/check').post((req, res) => {});
 
-    });
-    
-  app.route('/api/solve')
-    .post((req, res) => {
-
-    });
+  app.route('/api/solve').post((req, res) => {
+    console.log(req.body);
+    if (req.body.puzzle !== '' && req.body.hasOwnProperty('puzzle')) {
+      if (/^[\.\d]*$/.test(req.body.puzzle)) {
+        if (req.body.puzzle.length == 81) {
+          let solution = solver.solve(req.body.puzzle);
+          if (solution === 'not solvable') {
+            res.json({
+              error: 'Puzzle cannot be solved',
+            });
+          } else {
+            res.json({
+              solution: solver.solve(req.body.puzzle),
+            });
+          }
+        } else {
+          res.json({
+            error: 'Expected puzzle to be 81 characters long',
+          });
+        }
+      } else {
+        res.json({
+          error: 'invalid characters in puzzle',
+        });
+      }
+    } else {
+      res.json({
+        error: 'Required field missing',
+      });
+    }
+  });
 };
